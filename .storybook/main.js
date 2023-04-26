2115c1bc98ad57b68bedaa6cafd8302084d7af66
torvic <torvic02st96@gmail.com>
2023-04-26 16:02:09 -0500

    feat: add initial configuration to the project

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	staticDirs: ['../public'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
